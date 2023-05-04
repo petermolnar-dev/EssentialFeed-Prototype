3047b15dd910c0d005d0041aa768b8c671a0894b
Peter Molnar <petermolnar.hu@gmail.com>
2023-05-04 18:29:08 +0200

    Initial Commit

---
+++ Prototype/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prototype
+//
+//  Created by Peter Molnar on 04/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
